ee270bc5a683c425a60d566fe2f4da26822f0a9f
s-kreft <sebastian.kreft.sg@gmail.com>
2022-12-02 15:13:42 +0100

    Add service and method AddEmployer

---
+++ TmaLib/Services/AddEmployerService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TmaLib.Model;
+
+namespace TmaLib.Services
+{
+    public class AddEmployerService : IAddEmployerService
+    {
+        public List<Employer> Employers = new List<Employer>();
+        public Employer Employer { get; set; }
+
+        public AddEmployerService(UserInputAddEmployer UserInput)
+        {
+            Employer.Id = UserInput._id;
+            Employer.Name = UserInput._name;
+        }
+
+        public void AddEmployer()
+        {
+            Employers.Add(Employer);
+        }
+
+    }
+}
---
+++ TmaLib/UserInputAddEmployer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TmaLib.Model;
+
+namespace TmaLib
+{
+    public class UserInputAddEmployer
+    {
+        public long _id;
+        public string _name;
+        public UserInputAddEmployer(long Id, string name)
+        {
+            _id = Id;
+            _name = name;
+
+        }
+    }
+}
