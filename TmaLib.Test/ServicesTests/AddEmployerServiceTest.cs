01076d2418834a5b7cac765ca3efb28a7aca402e
s-kreft <sebastian.kreft.sg@gmail.com>
2022-12-02 15:13:42 +0100

    Add service and method AddEmployer

---
+++ TmaLib.Test/ServicesTests/AddEmployerServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TmaLib.Services;
+using TmaLib.Model;
+
+namespace TmaLib.Test.ServicesTests
+{
+    public class AddEmployerServiceTest
+    {
+        private readonly AddEmployerService _sut;
+
+        public AddEmployerServiceTest()
+        {
+            _sut = new AddEmployerService();
+        }
+
+        [Fact]
+        public void AddEmployerTest()
+        {
+            _sut.AddEmployer(1, "employer1");
+        }
+
+    }
+}
