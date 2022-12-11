0bbb056d0146ac4bee5d3ec36a4240d1f6982c46
s-kreft <sebastian.kreft.sg@gmail.com>
2022-12-11 14:58:45 +0100

    Add addemployer method with unit tests

--- TmaLib.Test/ServicesTests/AddEmployerServiceTest.cs
+++ TmaLib.Test/ServicesTests/AddEmployerServiceTest.cs
@@ -5,22 +5,67 @@ using System.Text;
 using System.Threading.Tasks;
 using TmaLib.Services;
 using TmaLib.Model;
+using System.Security.Cryptography.X509Certificates;
 
 namespace TmaLib.Test.ServicesTests
 {
     public class AddEmployerServiceTest
     {
         private readonly AddEmployerService _sut;
+        private readonly UserInputAddEmployer _userInputAddEmployer;
+        private readonly Employer _employer;
 
         public AddEmployerServiceTest()
         {
-            _sut = new AddEmployerService();
+            _userInputAddEmployer = new UserInputAddEmployer(1, "TestName1");
+            _employer = new Employer(_userInputAddEmployer); 
+            _sut = new AddEmployerService(_userInputAddEmployer);
+            
         }
 
         [Fact]
-        public void AddEmployerTest()
+        public void AddEmployerIsNullTest()
         {
-            _sut.AddEmployer(1, "employer1");
+            //Arrange
+
+            //Act
+            _sut.AddEmployer(_employer);
+
+            //Assert
+
+            var employerTest = _sut.Employers.FirstOrDefault();
+            Assert.NotNull(employerTest);
+            
+            
+            
+        }
+
+        [Fact]
+        public void AddEmployerAddIDTest()
+        {
+            //Arrange
+
+            //Act
+            _sut.AddEmployer(_employer);
+
+            //Assert
+            var employerTest = _sut.Employers.FirstOrDefault().Name;
+            Assert.Equal("TestName1", employerTest);
+
+        }
+
+        [Fact]
+        public void AddEmployerAddNameTest()
+        {
+            //Arrange
+
+            //Act
+            _sut.AddEmployer(_employer);
+
+            //Assert
+            var employerTest = _sut.Employers.FirstOrDefault();
+            Assert.Equal(1, _sut.Employers.FirstOrDefault().Id);
+
         }
 
     }
--- TmaLib/Services/AddEmployerService.cs
+++ TmaLib/Services/AddEmployerService.cs
@@ -4,23 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TmaLib.Model;
+using Xunit.Sdk;
 
 namespace TmaLib.Services
 {
     public class AddEmployerService : IAddEmployerService
     {
+        public Employer employer;
         public List<Employer> Employers = new List<Employer>();
-        public Employer Employer { get; set; }
+        
 
-        public AddEmployerService(UserInputAddEmployer UserInput)
+        public AddEmployerService(UserInputAddEmployer userInput)
         {
-            Employer.Id = UserInput._id;
-            Employer.Name = UserInput._name;
+            employer = new Employer(userInput);
+            employer.Id = userInput._id;
+            employer.Name = userInput._name;
         }
 
-        public void AddEmployer()
+        public void AddEmployer(Employer employer)
         {
-            Employers.Add(Employer);
+            Employers.Add(employer);
         }
 
     }
