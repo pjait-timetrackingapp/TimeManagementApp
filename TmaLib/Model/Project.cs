da2baaadaa110d272433209f9c02f49a32797ae4
s-kreft <sebastian.kreft.sg@gmail.com>
2022-12-19 20:53:41 +0100

    Add project class, projectService, user input for adding project

---
+++ TmaLib/Model/Project.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TmaLib.Model
+{
+    internal class Project
+    {
+        public long projectId { get; set; }
+        public string projectName { get; set; }
+
+        public Project()
+        {
+
+        }
+        public Project(UserInputAddProject userInputAddProject)
+        {
+            projectId= userInputAddProject.projectId;
+            projectName= userInputAddProject.projectName;
+        }
+    }
+}
