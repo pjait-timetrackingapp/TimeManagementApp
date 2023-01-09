79defd4fda52386cde2d27edd898f8f987d876d5
Damian Kreft <25139693+damiankreft@users.noreply.github.com>
2023-01-10 00:17:22 +0100

    TMA-3 Add form-backing fields to AddTimeEntryViewModel.

--- TimeManagementAppGui/ViewModel/AddTimeEntryViewModel.cs
+++ TimeManagementAppGui/ViewModel/AddTimeEntryViewModel.cs
@@ -2,9 +2,14 @@
 {
     public class AddTimeEntryViewModel
     {
+        public double Time { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public long ProjectId { get; set; }
+        public long EmployerId { get; set; }
+
         public AddTimeEntryViewModel()
         {
-
         }
     }
 }
