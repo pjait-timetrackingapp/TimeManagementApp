05f9dce7e08ede8057155f3727b3f006261040ef
Damian Kreft <25139693+damiankreft@users.noreply.github.com>
2023-01-13 17:50:38 +0100

    Add resolvable view routing.

--- TimeManagementAppGui/App.xaml.cs
+++ TimeManagementAppGui/App.xaml.cs
@@ -1,11 +1,21 @@
-﻿namespace TimeManagementAppGui;
+﻿using TimeManagementAppGui.View;
+
+namespace TimeManagementAppGui;
 
 public partial class App : Application
 {
 	public App()
 	{
-		InitializeComponent();
+		InitializeRouting();
+
+        InitializeComponent();
 
 		MainPage = new AppShell();
 	}
+
+	private static void InitializeRouting()
+	{
+        Routing.RegisterRoute("AddEntry", typeof(AddTimeEntry));
+		Routing.RegisterRoute("//Main/Calendar", typeof(MonthView));
+    }
 }
