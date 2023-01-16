8e1f4a5c36e4b82e4293bc93ab095076b782202a
Damian Kreft <25139693+damiankreft@users.noreply.github.com>
2023-01-16 15:59:51 +0100

    Display details on MonthCalendar's day tap.

--- TimeManagementAppGui/View/MonthView.xaml.cs
+++ TimeManagementAppGui/View/MonthView.xaml.cs
@@ -1,3 +1,4 @@
+using DevExpress.Maui.Scheduler;
 using TimeManagementAppGui.ViewModel;
 
 namespace TimeManagementAppGui.View;
@@ -10,4 +11,19 @@ public partial class MonthView : ContentPage
 		InitializeComponent();
         MonthScheduler.BindingContext = schedulerData;
 	}
+
+    private void MonthScheduler_Tap(object sender, SchedulerGestureEventArgs e)
+    {
+        if (e != null)
+        {
+            var appointments = AppointmentStorage.AppointmentItems.Where(a => a.Start == e.IntervalInfo.Start);
+            ShowAppointmentsPage(appointments);
+        }
+    }
+
+    private void ShowAppointmentsPage(IEnumerable<AppointmentItem> appointments)
+    {
+        var appEditPage = new AppointmentsPage(appointments);
+        Navigation.PushAsync(appEditPage);
+    }
 }
