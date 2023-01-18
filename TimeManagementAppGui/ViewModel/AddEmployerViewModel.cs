90e5e0df38af162c532b7885df0e6fef5651870d
Damian Kreft <25139693+damiankreft@users.noreply.github.com>
2023-01-18 20:39:26 +0100

    Create Employer creation page.

---
+++ TimeManagementAppGui/ViewModel/AddEmployerViewModel.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using TimeManagementAppGui.ViewModel.Base;
+using TimeManagementAppGui.ViewModel.Base.Dialog;
+using TimeManagementAppGui.ViewModel.Base.Navigation;
+using TmaLib.Services;
+
+namespace TimeManagementAppGui.ViewModel
+{
+    public partial class AddEmployerViewModel : ViewModelBase
+    {
+        [ObservableProperty]
+        private string name;
+        
+        private readonly IAddEmployerService _addEmployerService;
+
+        public AddEmployerViewModel(IDialogService dialogService, INavigationService navigationService, IAddEmployerService addEmployerService) : base(dialogService, navigationService)
+        {
+            _addEmployerService = addEmployerService;
+        }
+
+        [RelayCommand]
+        private void AddEmployer(string name)
+        {
+            _addEmployerService.AddEmployer(new TmaLib.UserInputAddEmployer(3, name));
+            NavigationService.NavigateToAsync("//Main/Calendar");
+        }
+    }
+}
