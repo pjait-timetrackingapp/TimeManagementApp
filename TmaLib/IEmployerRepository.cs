fd710ad0cf2af83c56e41eed1e8750bb093dd6d2
Damian Kreft <25139693+damiankreft@users.noreply.github.com>
2023-01-19 14:06:06 +0100

    Implement EmployeeRepository.

---
+++ TmaLib/IEmployerRepository.cs
@@ -0,0 +1,12 @@
+﻿using TmaLib.Model;
+
+namespace TmaLib
+{
+    public interface IEmployerRepository
+    {
+        Employer Add(Employer employer);
+        Task<Employer> GetById(int id);
+        Employer Remove(Employer employer);
+        Employer Update(Employer employer);
+    }
+}
